5dd009327c0a05007212650c1c91cca0845fd462
anujgunjad <a.gunjad11@gmail.com>
2022-03-25 13:13:25 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,23 @@ export class AppComponent implements OnDestroy {
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 1200px)');
+    console.log(this.mobileQuery);
+
+    //----------------------------------------------------------------//
+    // MediaMatcher - A utility for calling matchMedia queries.
+    // MediaMatcher.matchMedia -
+    // Evaluates the given media query and returns the native MediaQueryList
+    // from which results can be retrieved. Confirms the layout engine will trigger
+    // for the selector query provided and returns the MediaQueryList for the query provided.
+    //---------------------------------------------------------------//
+    // ChangeDetectorRef
+    // Base class that provides change detection functionality. A change-detection tree
+    // collects all views that are to be checked for changes.
+    //---------------------------------------------------------------//
+    // OnDestroy
+    // A lifecycle hook that is called when a directive, pipe, or service is destroyed. Use for any custom
+    // cleanup that needs to occur when the instance is destroyed.
+    //---------------------------------------------------------------//
 
     this._mobileQueryListener = () => {
       console.log('Mobile Query');
